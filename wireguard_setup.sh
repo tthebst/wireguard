38fe30a0bf5b0a45401e5393e887fc4b1da9abf1
tthebst <gretler.tim@gmail.com>
2019-12-27 21:43:46 +0100

    still not woriking

--- wireguard_setup.sh
+++ wireguard_setup.sh
@@ -4,8 +4,13 @@
 
 
 #create google cloud micro instance
-chmod 777 /home/gcloud/wireguard.pub
-gcloud beta compute --project=personal-stuff123 instances create instance-wire --zone=asia-east2-c --machine-type=f1-micro --subnet=default --network-tier=PREMIUM --metadata-from-file ssh-keys=/home/gcloud/wireguard.pub --maintenance-policy=MIGRATE --service-account=356728976673-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --image=ubuntu-1804-bionic-v20191113 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=instance-1 --reservation-affinity=any
+ssh-keygen -q -N "" -t rsa -f ~/.ssh/gcloud -C tim
+echo -n "tim:" > $HOME/id_rsa.pub
+cat $HOME/id_rsa.pub
+cat $HOME/.ssh/gcloud.pub >> $HOME/id_rsa.pub
+cat $HOME/id_rsa.pub
+ls ~/.ssh/ 
+gcloud beta compute --project=personal-stuff123 instances create instance-wire --zone=asia-east2-c --machine-type=f1-micro --subnet=default --network-tier=PREMIUM --metadata-from-file ssh-keys=$HOME/id_rsa.pub --maintenance-policy=MIGRATE --service-account=356728976673-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --image=ubuntu-1804-bionic-v20191113 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=instance-1 --reservation-affinity=any
 
 
 #get IP address of created instance
@@ -17,8 +22,8 @@ echo "${IP}"
 echo timgretler@${IP}
 ls /home/gcloud/wireguard
 ls /home
-scp  -v -i /home/gcloud/wireguard /home tim@${IP}:/home/wireguard
-ssh -v -i /home/gcloud/wireguard tim@${IP} 'sudo add-apt-repository ppa:wireguard/wireguard;sudo apt-get update;sudo apt-get install wireguard;wg genkey | tee privatekey | wg pubkey > publickey;echo privatekey;python3 /home/create_server_file.py privatekey "$(echo "/home/wireguard/gcloud/wireguard.pub")" ;wg-quick up /home/wireguard/server.conf'
+scp  -v -i $HOME/.ssh/gcloud /home tim@${IP}:/home/wireguard
+ssh -v -i $HOME/.ssh/gcloud tim@${IP} 'sudo add-apt-repository ppa:wireguard/wireguard;sudo apt-get update;sudo apt-get install wireguard;wg genkey | tee privatekey | wg pubkey > publickey;echo privatekey;python3 /home/create_server_file.py privatekey "$(echo "/home/wireguard/gcloud/wireguard.pub")" ;wg-quick up /home/wireguard/server.conf'
 
 
 
